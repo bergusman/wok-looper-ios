4de4131aa6bf399697886e108bf871053f7ac4eb
Vitaly Berg <bergusman@gmail.com>
2021-07-20 19:22:42 +0800

    Play/pause on view controller lifecycle

--- Looper/HomeViewController.swift
+++ Looper/HomeViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var coinView: VideoLooperView!
     @IBOutlet weak var letterView: VideoLooperView!
     @IBOutlet weak var topView: VideoLooperView!
     private var looperViews: [VideoLooperView] = []
+    
+    private func setup() {
+        looperViews = [
+            coinView,
+            letterView,
+            topView
+        ]
+    }
+    
+    private func play() {
+        for (i, looperView) in self.looperViews.enumerated() {
+            if i == segmentedControl.selectedSegmentIndex {
+                looperView.play()
+            }
+        }
+    }
+    
+    private func pause() {
+        looperViews.forEach { $0.pause() }
+    }
 
     @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
         for (i, looperView) in self.looperViews.enumerated() {
@@ -40,12 +61,17 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        looperViews = [
-            coinView,
-            letterView,
-            topView
-        ]
-        coinView.play()
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        play()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        pause()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
