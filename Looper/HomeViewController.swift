6f792b3c0902584efc56f4d05428e7fc88f06861
Vitaly Berg <bergusman@gmail.com>
2021-07-20 19:06:40 +0800

    Create sample

--- Looper/HomeViewController.swift
+++ Looper/HomeViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var coinView: VideoLooperView!
+    @IBOutlet weak var letterView: VideoLooperView!
+    @IBOutlet weak var topView: VideoLooperView!
+    private var looperViews: [VideoLooperView] = []
 
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        for (i, looperView) in self.looperViews.enumerated() {
+            if i == sender.selectedSegmentIndex {
+                looperView.play()
+            }
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.coinView.alpha = sender.selectedSegmentIndex == 0 ? 1 : 0
+            self.letterView.alpha = sender.selectedSegmentIndex == 1 ? 1 : 0
+            self.topView.alpha = sender.selectedSegmentIndex == 2 ? 1 : 0
+        } completion: { (finished) in
+            if finished {
+                for (i, looperView) in self.looperViews.enumerated() {
+                    if i != sender.selectedSegmentIndex {
+                        looperView.pause()
+                    }
+                }
+            }
+        }
+    }
+    
     // MARK: - UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        looperViews = [
+            coinView,
+            letterView,
+            topView
+        ]
+        coinView.play()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
--- Looper/VideoLooperView.swift
+++ Looper/VideoLooperView.swift
@@ -7,7 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VideoLooperView: UIView {
-
+    @IBInspectable var videoName: String?
+    
+    // MARK: - Player
+    
+    private var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
+    private var player: AVQueuePlayer!
+    private var playerLooper: AVPlayerLooper!
+    private var playerItem: AVPlayerItem!
+    
+    func play() {
+        player.play()
+    }
+    
+    func pause() {
+        player.pause()
+    }
+    
+    // MARK: - UIView
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        player = AVQueuePlayer()
+        playerLayer.videoGravity = .resizeAspect
+        playerLayer.player = player
+        
+        if let video = videoName, let url = Bundle.main.url(forResource: video, withExtension: nil) {
+            playerItem = AVPlayerItem(url: url)
+            playerLooper = AVPlayerLooper(player: player, templateItem: playerItem)
+        }
+    }
+    
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
 }
